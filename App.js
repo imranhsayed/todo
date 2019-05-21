da6926fe3340ef4a22ec292224791d03fee43dd7
Imran Sayed <imranhsayed@gmail.com>
2019-05-22 00:08:32 +0530

    Change the App class component into functional component

--- App.js
+++ App.js
@@ -1,11 +1,6 @@
-import React, {Component} from 'react';
+import React from 'react';
 import Todo from './src/app/Todo';
 
-type Props = {};
-export default class App extends Component<Props> {
-  render() {
-    return (
-		<Todo/>
-    );
-  }
-}
+const App = () => <Todo/>;
+
+export default App;
