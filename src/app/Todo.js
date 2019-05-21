f384f8f21eb2ab911c62f56c36f417b041df90d4
Imran Sayed <imranhsayed@gmail.com>
2019-05-22 00:42:27 +0530

    Initialize State and render the todo list items by mapping throught the array

--- src/app/Todo.js
+++ src/app/Todo.js
@@ -8,10 +8,49 @@ import  {
 } from 'react-native';
 
 class Todo extends React.Component {
+
+	constructor( props ) {
+		super( props );
+
+		this.state = {
+			todos: [],
+			newTodo: '',
+		}
+	};
+
+	handleOnChangeText = ( event ) => {
+		this.setState( { newTodo: event.target.value } );
+	};
+
+	handlePress = ( event ) => {
+
+	};
+
 	render() {
+
+		const { todos, newTodo } = this.state;
+
 		return(
 			<View>
-				<Text>Hello World</Text>
+				{/*Text Input*/}
+				<TextInput
+					value={newTodo}
+					onChangeText={this.handleOnChangeText}
+				/>
+				<TouchableOpacity onPress={ this.handlePress }>
+					<Text>Create</Text>
+				</TouchableOpacity>
+
+				{/*	Todos */}
+				{ todos.length ? (
+					<View>
+						{ todos.map( ( item, index ) => (
+							<Text key={ `${item}-${index}` }>
+								{ item }
+							</Text>
+						) ) }
+					</View>
+				) : false }
 			</View>
 		)
 	}
